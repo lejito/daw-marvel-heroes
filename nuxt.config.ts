fb6e636458086802aff57e96bae7ec07ea7d5445
Lejito <alejocrrs@gmail.com>
2023-09-15 15:54:01 -0500

    Fix template project from Vercel

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,4 @@
-export default {
-  nitro: {
-    preset: 'vercel-edge',
-  },
-};
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true }
+})
